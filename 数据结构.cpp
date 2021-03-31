21c98f1442c0ab78a0c6155b5cfa8b60c7caf83d
DMoRanSky <85811545@qq.com>
2021-03-31 21:36:00 +0800

    qwq auto save

--- 数据结构.cpp
+++ 数据结构.cpp
@@ -583,3 +583,59 @@ namespace DLX2{
     for (int i = 1; i <= dep; i++) printf("%d ", ans[i]);
 
 }
+
+
+typedef pair<LL, LL> PII;
+typedef set<PII>::iterator SIT;
+typedef set<PII> SI;
+
+PII operator - (const PII &a, const PII &b) {
+	return mp(a.x - b.x, a.y - b.y);
+}
+
+LL inline cross(PII a, PII b) {
+	return a.x * b.y - a.y * b.x;
+}
+
+LL inline cross(PII a, PII b, PII c) {
+	PII u = b - a, v = c - a;
+	return cross(u, v);
+}
+
+// 动态平衡树
+
+struct Hull {
+	SI su, sd;
+	bool inline query(SI &s, PII u, int o) {
+		SIT l = s.upper_bound(u), r = s.lower_bound(u);
+		if (r == s.end() || l == s.begin()) return false;
+		l--;
+		return cross(*l, u, *r) * o <= 0;
+	}
+	void inline insert(SI &s, PII u, int o) {
+		if (query(s, u, o)) return;
+		SIT it = s.insert(u).first;
+		while (1) {
+			SIT mid = it;
+			if (mid == s.begin()) break; --mid;
+			SIT l = mid;
+			if (l == s.begin()) break; --l;
+			if (cross(*l, *mid, u) * o >= 0) break; 
+			s.erase(mid);
+		}
+		while (1) {
+			SIT mid = it; ++mid;
+			if (mid == s.end()) break; 
+			SIT r = mid; ++r;
+			if (r == s.end()) break;
+			if (cross(u, *mid, *r) * o >= 0) break; 
+			s.erase(mid);
+		}
+	}
+	void inline ins(PII u) {
+		insert(su, u, 1), insert(sd, u, -1);
+	}
+	int inline chk(PII u) {
+		return query(su, u, 1) && query(sd, u, -1);
+	}
+} t;
